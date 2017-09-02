3667298b127e23cc331edc1c4ed10a323d545b48
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-09-01 18:43:23 -0700

    +Meal

---
+++ Backpack_Buddy/Entities/Meal.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Backpack_Buddy.Entities
+{
+    class Meal
+    {
+        String Name;
+        List<FoodItem> Ingredients;
+        LinkedList<string> Steps;
+    }
+}
