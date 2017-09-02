deff37a587a9fab3c3b3407d601251c73c0b692d
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-09-01 21:22:54 -0700

    Added Day and Meal Options to Meal

--- Backpack_Buddy/Entities/Meal.cs
+++ Backpack_Buddy/Entities/Meal.cs
@@ -4,7 +4,26 @@ namespace Backpack_Buddy.Entities
 {
     class Meal
     {
+        enum Day
+        {
+            Sunday,
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        }
+        enum Meal
+        {
+          Breakfast,
+          Lunch,
+          Dinner
+        }
+
         String Name;
+        Day Day;
+        Meal Meal;
         List<FoodItem> Ingredients;
         LinkedList<string> Steps;
     }
