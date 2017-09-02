1883edcc117fdc8ceb61408c8d786ba0f167e89c
MasterChief-John-117 <MasterChief-John-117@users.noreply.github.com>
2017-09-01 22:11:36 -0700

    +Trip

---
+++ Backpack_Buddy/Entities/Trip.cs
@@ -0,0 +1,14 @@
+﻿namespace Backpack_Buddy.Entities
+{
+    public class Trip
+    {
+        string Name;
+        Location Location;
+        Location DepartureLocation;
+        Camper Organzier;
+        List<Camper> Campers;
+        List<Meal> Menue;
+        DateTime StartDate;
+        DateTime EndDate;
+    }
+}
